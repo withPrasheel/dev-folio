8902b5935c46754db5eefdac856abdd26c4bd4cc
Prasheel <prasheelnandwana3@gmail.com>
2024-07-17 20:14:07 -0700

    Changes to tools and techstack

--- src/components/About/Timeline.js
+++ src/components/About/Timeline.js
@@ -1,16 +1,36 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Timeline.css'; // Make sure to create a Timeline.css file
 
 function Timeline() {
   return (
-    <Card className="quote-card-view">
-      <Card.Body>
-        <blockquote className="blockquote mb-0">
-          Timeline to be added
-        </blockquote>
-      </Card.Body>
-    </Card>
+    <div className="timeline-container">
+      <div className="timeline">
+        <div className="timeline-item left">
+          <Card className="timeline-card education-card">
+            <Card.Body>
+              <Card.Title>Education</Card.Title>
+              <Card.Text>
+                Bachelor of Science in Computer Science
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="timeline-line"></div>
+        <div className="timeline-item right">
+          <Card className="timeline-card experience-card">
+            <Card.Body>
+              <Card.Title>Experience</Card.Title>
+              <Card.Text>
+                Software Engineer at XYZ Company
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    </div>
   );
 }
 
